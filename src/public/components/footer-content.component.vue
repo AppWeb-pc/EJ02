a5eef9e7929de6fd52ce29c285a0129470b6d32b
IanHD04 <u202214059@upc.edu.pe>
2024-04-17 13:32:39 -0500

    Create footer

--- src/public/components/footer-content.component.vue
+++ src/public/components/footer-content.component.vue
@@ -1,11 +1,23 @@
-<script setup>
-
+<script >
+export default {
+  name:'footer-content'//Se pone el nombre que le pongas al componente
+}
 </script>
 
 <template>
-
+  <div class="footer">
+    <!-- Cambiar segun lo que pidan -->
+    <p>Copyright &copy;  2024 HIPO, inc All rights reserved.</p>
+    <p><b>Developed by: Ian Haziel Donato Santisteban Palomino - u202214059</b></p>
+  </div>
 </template>
 
 <style scoped>
+.footer{
+  padding: 20px;
+  text-align: center;
+  background-color: #000000;
+  color: #ffffff;
+}
 
 </style>
