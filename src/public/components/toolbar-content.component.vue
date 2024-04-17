4ca038c7cb5f73b511c9477691e77f15db34da7e
IanHD04 <u202214059@upc.edu.pe>
2024-04-17 13:37:31 -0500

    Create toolbar

---
+++ src/public/components/toolbar-content.component.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: 'toolbar-content',
+};
+</script>
+
+<template>
+  <pv-toolbar class="toolbar">
+    <template #start></template>
+    <template #center>
+      <h1>HIPO Universities API Service</h1>
+    </template>
+    <template #end></template>
+  </pv-toolbar>
+</template>
+
+<style scoped>
+.toolbar {
+  background-color: #000000;
+  color: white;
+  padding: 1.5rem;
+  text-align: center;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+.toolbar h1 {
+  margin: 0;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: white;
+}
+</style>
